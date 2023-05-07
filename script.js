131f6d33de1891475609184689c815a16af8d56a
Jordan <j.koevoets@student.fontys.nl>
2023-05-07 20:02:39 +0200

    Basis menu collapseable

---
+++ script.js
@@ -0,0 +1,35 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "calc(200px + 10%)";
+}
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
+
+var coll = document.getElementsByClassName("kopje");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
