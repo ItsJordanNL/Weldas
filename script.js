4efa75a05f2e9df325ca3bf8b977c6999396c175
Jordan <j.koevoets@student.fontys.nl>
2023-05-08 10:57:05 +0200

    Menu met correcte items

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "calc(200px + 10%)";
+  document.getElementById("mySidepanel").style.width = "calc(200px + 10%)";
 }
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.maxHeight){
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
-      content.style.maxHeight = content.scrollHeight + "px";
+      content.style.maxHeight = content.scrollHeight + "0px";
     }
   });
 }
 function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
+  document.getElementById("mySidepanel").style.width = "0";
 }
 
 var coll = document.getElementsByClassName("kopje");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.maxHeight){
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
